11cbf255ddbaa08a48e0b40f1597cfc43cc8584e
RobertZK <technoguyrob@gmail.com>
2014-05-31 17:25:25 -0500

    updates for syberia stages (just copied over from syberia main)

--- R/data_stage.r
+++ R/data_stage.r
@@ -8,11 +8,18 @@
 #' @export
 data_stage <- function(modelenv, munge_procedure) {
   # preprocess_munge_procedure(munge_procedure)
+  removed_steps <- vapply(munge_procedure, is.trigger, logical(1))
   # TODO: sameAs/importFrom and butWith/except triggers
   # TODO: save trigger
 
   stagerunner <- munge(modelenv, munge_procedure,
-                       stagerunner = list(remember = TRUE))
+    stagerunner = list(remember = TRUE),
+    train_only = TRUE # This refers to us not wanting to set the mungebits
+                      # as "trained" when running this stageRunner, since we
+                      # would like to be able to run them multiple times -- 
+                      # the stageRunner used in the main syberia model run is
+                      # only used for training.
+  ) 
     
   stagerunner
 }
