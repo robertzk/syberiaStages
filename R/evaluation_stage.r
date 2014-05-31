c7b5f31b36ec5a94489cd11e8f74e6cc907f5975
RobertZK <technoguyrob@gmail.com>
2014-05-31 18:23:19 -0500

    refactor option generation in evaluation stage

--- R/evaluation_stage.r
+++ R/evaluation_stage.r
@@ -4,51 +4,66 @@
 #' model according to the metrics: AUC, a confusion matrix, and a validation plot.
 #'
 #' The evaluation stage parameters in the syberia model file are described in the
-#' `evaluation_parameters` argument below.
+#' evaluation_parameters argument.
 #'
 #' @param modelenv an environment. The persistent modeling environment.
 #' @param evaluation_parameters list. These come from the syberia model and
 #'    must be in the following format:
 #'
 #'    \itemize{
-#'      \item type. The first element must be a string `"regression"` or "`classification"`.
+#'      \item type. The first element must be a string "regression" or "classification".
 #'      \item percent. The percent of the data that was used for training. Currently,
 #'        only sequential splits of training and validation are supported until
-#'        syberia introduces a better mechanism for data partitions.
+#'        syberia introduces a better mechanism for data partitions. The default is 0.8.
+#'      \item dep_var. The name of the dependent variable in the evaluated data.
+#'        The default is "dep_var".
+#'      \item prediction_output. The name of the CSV to which to output the results
+#'        of the validation. This will be a data.frame with the columns "dep_var",
+#'       "loan_id", and "score", where the latter refers to predicted score.
+#'      \item cutoff. A cutoff for binary classification predictions: above the
+#'        cutoff means a prediction 1, and otherwise a 0. The default is 0.5.
 #'     }
 #' @export
 #' @return a stageRunner that performs AUC, confusing matrix, and validation
 #'   plotting.
-evaluation_stage <- function(modelenv, evaluation_parameters) {
-  # TODO: (RK) Remove this to use the IO adapter once that has been written.
-  # In order to grab the data as what it looked like prior to any data preparation,
-  # we are going to extract it from the cached environment of the first step in the
-  # data stage. This way, it will be import-method-agnostic, and we will not
-  # have to worry whether our data came from CSV, S3, etc. We also assume the
-  # stageRunner we are attached to is in `active_runner()`.
-  raw_data <- stagerunner:::treeSkeleton(
-    active_runner()$stages$data)$first_leaf()$object$cached_env$data
+evaluation_stage <- function(evaluation_parameters) {
+  # Only binary classification and continuous regression are supported
+  # evaluation types for now.
+  stopifnot((is.character(type <- evaluation_parameters[[1]]) ||
+             is.character(type <- evaluation_parameters$type)) &&
+            type %in% c("regression", "classification"))
 
-  stopifnot(is.character(evaluation_parameters[[1]]) &&
-            evaluation_parameters[[1]] %in% c("regression", "classification"))
-  # if (!exists(evalutaion_fn <- pp('eval_models_#{evaluation_parameters[[1]]}')))
-  #  stop("Missing evaluation container for keyword '", evaluation_parameters[[1]], "'")
-  trainpct <- evaluation_parameters[[grep("percent", names(evaluation_parameters))]]
-  test_rows <- (trainpct*nrow(modelenv$data)+1) : nrow(modelenv$data)
-  test_data <- modelenv$data[test_rows, ]
-  score <- modelenv$model_stage$model$predict(test_data)
+  cutoff <- evaluation_parameters$cutoff %||% 0.5
+  list(
+       plot.it = TRUE, xlab = c("dep_var = 0", "dep_var = 1"),
+       ylab = c("score = 0", "score = 1"), title = NULL, last = FALSE, nxt = NULL)
+
+  generate_options <- function(modelenv) {
+    # TODO: (RK) Remove this to use the IO adapter once that has been written.
+    # In order to grab the data as what it looked like prior to any data preparation,
+    # we are going to extract it from the cached environment of the first step in the
+    # data stage. This way, it will be import-method-agnostic, and we will not
+    # have to worry whether our data came from CSV, S3, etc. We also assume the
+    # stageRunner we are attached to is in `active_runner()`.
+    raw_data <- stagerunner:::treeSkeleton(
+      active_runner()$stages$data)$first_leaf()$object$cached_env$data
+
+    train_percent <- evaluation_parameters$percent %||% 0.8
+    validation_rows <- seq(train_percent * nrow(raw_data) + 1, nrow(raw_data))
+    validation_data <- raw_data[validation_rows, ]
+    score <- modelenv$model_stage$model$predict(validation_data)
+
+    modelenv$evaluation_stage$prediction_data <-
+      data.frame(dep_var = test_data[[evaluation_parameters$dep_var %||% 'dep_var']],
+                 score = score, loan_id = validation_data$loan_id)
+
+    if ('prediction_output' %in% names(evaluation_parameters)) {
+      write.csv(prediction, evaluation_parameters$prediction_output, row.names = FALSE)
+    }
+  }
   
-  #pred_scores <- global_modeling_environment$model_stage$model$predict(global_modeling_environment$data
-  prediction <- data.frame(dep_var = test_data[[evaluation_parameters$dep_var]], score = score)
-  write.csv(cbind(test_data$loan_id, prediction), gsub(".csv", "_prediction.csv", global_modeling_environment$import_stage$file), row.names = F)
-  modelenv$evaluation_stage$options <-
-    list(prediction = prediction, cutoff = evaluation_parameters$cutoff %||% 0.5,
-         plot.it = TRUE, xlab = c("dep_var = 0", "dep_var = 1"),
-         ylab = c("score = 0", "score = 1"), title = NULL, last = FALSE, nxt = NULL)
-  #everything each function needs should reside in the modelenv$evalutaion_stage
-  
-  auc <- function(modelenv) { #each of the function is a stagerunner and it should just take modelenv as input
-    library(AUC)
+  auc <- function(modelenv) {
+    require(AUC)
     auc_result <- with(modelenv$evaluation_stage$options$prediction, {
       list(
         roc = AUC::auc(roc(score, factor(dep_var))),
