4b51d1157a7f80ab09fa8e17f25a82304e761026
Ryland Ely <ryland.ely@avantcredit.com>
2014-06-26 14:45:04 -0500

    roughed in "explore stage"

---
+++ R/explore_stage.r
@@ -0,0 +1,31 @@
+#' @export
+explore_stage <- function(modelenv,pars) {
+  stagerunner_input <- list(
+    "DQ Check"  = dqcheck(pars)
+  ) 
+}
+
+# We want to see how well the data are segmented by each variable in turn.
+# If the p-value is unrealistically low, it could be a sign that there is
+# some issue with the data.
+dqcheck <- function(pars) {
+  force(pars)
+  function(modelenv) {
+    whichones <- modelenv$data[[pars$depvarname]]==1
+    ones <- subset(modelenv$data,subset=whichones)
+    zeros <- subset(modelenv$data,subset=!whichones)
+    for (col in colnames(modelenv$data)) {
+      cat(col,": ",sep='')
+      if (is.factor(modelenv$data[[col]])) {
+        cat("is factor\n")
+      } else { 
+        mean1 <- formatC(mean(ones[[col]],na.rm=T),digits=3)
+        mean0 <- formatC(mean(zeros[[col]],na.rm=T),digits=3)
+        missing1 <- sum(is.na(ones[[col]]))
+        missing0 <- sum(is.na(zeros[[col]]))
+        cat("Avg[",col," | ",pars$depvarname,"=0] = ",mean0," (",missing0," NA), ",sep='')
+        cat("Avg[",col," | ",pars$depvarname,"=1] = ",mean1," (",missing1," NA)\n",sep='')
+      }
+    }
+  }
+}
