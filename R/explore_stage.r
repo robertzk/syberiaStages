d2f597b8d7ff773b73743094ed71f485fddbe1c8
Ryland Ely <ryland.ely@avantcredit.com>
2014-06-26 19:05:23 -0500

    Added simple data quality check to explore stage

--- R/explore_stage.r
+++ R/explore_stage.r
@@ -10,7 +10,7 @@ explore_stage <- function(modelenv,pars) {
 visualize <- function() {
   function(modelenv) {
     source("~/dev/avant-models/scripts/visualizeData.R")
-    plotHistograms(modelenv$data)
+    #plotHistograms(modelenv$data)
   }
 }
 
@@ -27,13 +27,40 @@ dqcheck <- function(pars) {
       cat(col,": ",sep='')
       if (is.factor(modelenv$data[[col]])) {
         cat("is factor\n")
+      } else if (is.character(modelenv$data[[col]])) {
+        cat("is character\n")
       } else { 
-        mean1 <- formatC(mean(ones[[col]],na.rm=T),digits=3)
-        mean0 <- formatC(mean(zeros[[col]],na.rm=T),digits=3)
-        missing1 <- sum(is.na(ones[[col]]))
-        missing0 <- sum(is.na(zeros[[col]]))
-        cat("Avg[",col," | ",pars$depvarname,"=0] = ",mean0," (",missing0," NA), ",sep='')
-        cat("Avg[",col," | ",pars$depvarname,"=1] = ",mean1," (",missing1," NA)\n",sep='')
+
+        x <- ones[[col]]
+        y <- zeros[[col]]
+
+        flag.problem = FALSE
+ 
+        # Check for unrealistically good separation of 0s and 1s via two-sample t-test
+        spaces <- paste0(rep(' ',nchar(col)+2),collapse='')
+        print.spaces <- function() ifelse(flag.problem, spaces, '')
+        if (all(is.na(x))) { cat(print.spaces(),'\033[0;31mAll 1s are missing\033[0m\n',sep=''); flag.problem=TRUE }
+        if (all(is.na(y))) { cat(print.spaces(),'\033[0;31mAll 0s are missing\033[0m\n',sep=''); flag.problem=TRUE } 
+        if (flag.problem) next
+        if (sum(is.na(x))==1) { cat(print.spaces(),'\033[0;31mOnly one non-missing 1\033[0m\n',sep=''); flag.problem=TRUE }
+        if (sum(is.na(y))==1) { cat(print.spaces(),'\033[0;31mOnly one non-missing 0\033[0m\n',sep=''); flag.problem=TRUE }
+        if (flag.problem) next
+        if (sd(x,na.rm=T)==0) { cat(print.spaces(),'\033[0;31m1s are single-valued\033[0m\n',sep=''); flag.problem=TRUE }
+        if (sd(y,na.rm=T)==0) { cat(print.spaces(),'\033[0;31m0s are single-valued\033[0m\n',sep=''); flag.problem=TRUE }
+        if (flag.problem) next
+
+        # Remove NAs
+        x <- x[!is.na(x)]
+        y <- y[!is.na(y)]
+ 
+        # Do the t-test and print results
+        tmp <- t.test(x,y)
+        p <- tmp$p.value
+        if (p<0.05) {
+          cat('\033[0;31mp-value = ',p,'\033[0m\n',sep='')
+        } else {
+          cat("p-value = ",p,'\n',sep='')
+        }
       }
     }
   }
