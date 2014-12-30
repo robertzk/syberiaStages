3d762b0ae5c12010a893e82f1ebbb44fd01e3f14
Fei Ye <fei.ye@avantcredit.com>
2014-12-30 10:09:27 -0600

    finalize change

--- R/adapter.r
+++ R/adapter.r
@@ -232,21 +232,20 @@ construct_s3_adapter <- function() {
   write_function <- function(object, opts) {
     common_s3mpi_package_loader()
 
-    obj <- object
     if (name_exists("object$output$options$data")) {
       data_restore_on_exit <- object$output$options$data
       on.exit(object$output$options$data <- data_restore_on_exit, add = TRUE)
-      obj$output$options$data <- NULL
+      object$output$options$data <- NULL
     }
     if (name_exists("object$output$options$label")) {
       label_restore_on_exit <- object$output$options$label
       on.exit(object$output$options$label <- label_restore_on_exit, add = TRUE)
-      obj$output$options$label <- NULL
+      object$output$options$label <- NULL
     }
 
     # If the user provided an s3 path, like "s3://somebucket/some/path/", 
     # pass it along to the s3read function.
-    args <- list(obj = obj, name = opts$resource)
+    args <- list(obj = object, name = opts$resource)
     if (is.element('s3path', names(opts))) args$.path <- opts$s3path
     do.call(s3mpi::s3store, args)
   }
